eafc957ad163321e3de4c8199a2475333328eb60
Aleksandr Kolesov <aleksandrkolesov@Aleksandrs-MacBook-Pro.local>
2025-11-03 11:28:48 +0100

    fix server playwright issue

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,6 @@
 import { defineConfig, defineProject } from "vitest/config";
 import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
-import path from "node:path";
-import { fileURLToPath } from "node:url";
-import { storybookTest } from "@storybook/addon-vitest/vitest-plugin";
-import { playwright } from "@vitest/browser-playwright";
-const dirname =
-  typeof __dirname !== "undefined"
-    ? __dirname
-    : path.dirname(fileURLToPath(import.meta.url));
-
 // More info at: https://storybook.js.org/docs/next/writing-tests/integrations/vitest-addon
 export default defineConfig({
   plugins: [react()],
